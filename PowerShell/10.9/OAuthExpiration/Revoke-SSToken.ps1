871e0621df9fbc220efdaf020940314760c4b2d7
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-15 10:57:31 -0600

    Create Revoke-SSToken.ps1

---
+++ PowerShell/10.9/OAuthExpiration/Revoke-SSToken.ps1
@@ -0,0 +1,35 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Expire OAuth Token
+###########################################################################################################################################################
+
+Function Revoke-SSToken {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+
+    $TokenRevoked = Invoke-RestMethod "$URI/api/v1/oauth-expiration" -Method Post -Headers $headers
+    return $TokenRevoked
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: $($responseBody.error)"
+        return;
+    }
+}
