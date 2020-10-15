f91914376f8ef8a7368cfca38d09bad95e96e349
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-15 10:39:33 -0600

    Create Get-SSRunBackup.ps1

---
+++ PowerShell/10.9/Configuration/Get-SSRunBackup.ps1
@@ -0,0 +1,36 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Run backup manually inside Secret Server
+###########################################################################################################################################################
+
+Function Get-SSRunBackup {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+    
+
+    $diagnostics = Invoke-RestMethod "$URI/api/v1/configuration/backup/run-now" -Method Post -Headers $headers
+    return $diagnostics
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: $($responseBody.error)"
+        return;
+    }
+}
