6d5854e47fd998f8dacb571bb6009131fed9a317
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-15 11:10:16 -0600

    Adding Scripts

---
+++ PowerShell/10.9/Folders/Remove-SSFolderByID.ps1
@@ -0,0 +1,39 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Delete folder by ID
+###########################################################################################################################################################
+
+Function Remove-SSFolderByID {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey,
+         [Parameter(Mandatory=$true, Position=2)]
+         [string] $FolderID
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+
+    $URL = "$URI/api/v1/folders/$FolderID"
+
+    $FolderResults = Invoke-RestMethod $URL -Method Delete -Headers $headers
+    return $FolderResults
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
---
+++ PowerShell/10.9/HealthCheck/Get-SSHealthCheck.ps1
@@ -0,0 +1,31 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Do a health check of the application.  This is an un-authenticated call.
+###########################################################################################################################################################
+
+Function Get-SSHealthCheck {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI
+    )
+
+    try
+    {
+
+    $options = Invoke-RestMethod "$URI/api/v1/healthcheck" -Method Get
+    return $options
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
