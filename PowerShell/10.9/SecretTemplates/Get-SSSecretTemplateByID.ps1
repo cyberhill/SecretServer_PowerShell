de649e4f396b15a7ea24956302c83fd79ea77cff
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-15 10:52:52 -0600

    Create Get-SSSecretTemplateByID.ps1

---
+++ PowerShell/10.9/SecretTemplates/Get-SSSecretTemplateByID.ps1
@@ -0,0 +1,39 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Get secret template by ID
+###########################################################################################################################################################
+
+Function Get-SSSecretTemplateByID {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey,
+         [Parameter(Mandatory=$true, Position=2)]
+         [string] $SecretTemplateID
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+
+    $URL = "$URI/api/v1/secret-templates/$SecretTemplateID"
+
+    $SecretTemplateDetails = Invoke-RestMethod $URL -Headers $headers
+    return $SecretTemplateDetails
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
