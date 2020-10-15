c9c1e36719973b7e974903162e7a8ac44a35c32c
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-15 10:55:59 -0600

    Create Get-SSPBAConfig.ps1

---
+++ PowerShell/10.9/PBAConfiguration/Get-SSPBAConfig.ps1
@@ -0,0 +1,35 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Get the Privilege Behavior Analytics configuration
+###########################################################################################################################################################
+
+Function Get-SSPBAConfig {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+    
+    $PBAConfig = Invoke-RestMethod "$URI/api/v1/pba-configuration" -Method Get -Headers $headers
+    return $PBAConfig
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: $($responseBody.error)"
+        return;
+    }
+}
