23636aa486f5d431666cffa454dbee60138ff3d6
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-15 10:48:57 -0600

    Create Get-SSNodes.ps1

---
+++ PowerShell/10.9/ServerNodes/Get-SSNodes.ps1
@@ -0,0 +1,36 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/15/2020
+# Description: Get all Secret Server web nodes
+###########################################################################################################################################################
+
+Function Get-SSNodes {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+    
+
+    $diagnostics = Invoke-RestMethod "$URI/api/v1/nodes" -Method Get -Headers $headers
+    return $diagnostics
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
