923db49c34e97f484b9b6fc61819680f5334744b
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-26 19:17:51 -0600

    Create Get-SSCurrentUser.ps1

---
+++ PowerShell/10.9/Users/Get-SSCurrentUser.ps1
@@ -0,0 +1,35 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/26/2020
+# Description: Get details of current Secret Server user
+###########################################################################################################################################################
+
+Function Get-SSCurrentUser {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+    
+    $secrets = Invoke-RestMethod $URI/api/v1//users/current -Method Get -Headers $headers
+    return $secrets
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
