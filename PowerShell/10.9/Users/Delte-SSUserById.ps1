33e4c50845d11040ada32e0d89716387d4ee6c54
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2020-10-26 19:39:33 -0600

    Create Delte-SSUserById.ps1

---
+++ PowerShell/10.9/Users/Delte-SSUserById.ps1
@@ -0,0 +1,37 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 10/26/2020
+# Description: Delete Secret Server user by ID
+###########################################################################################################################################################
+
+Function Delete-SSUserById {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey,
+         [Parameter(Mandatory=$true, Position=2)]
+         [string] $UserID
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+    
+    $secrets = Invoke-RestMethod $URI/api/v1/users/$UserID -Method Delete -Headers $headers
+    return $secrets
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
