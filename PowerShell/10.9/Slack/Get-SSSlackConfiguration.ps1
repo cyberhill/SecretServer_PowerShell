01ff86764a3e5e84c9d6478524a20ea96f78a7af
teddy141345 <48530026+teddy141345@users.noreply.github.com>
2021-02-10 19:45:36 -0700

    Create Get-SSSlackConfiguration.ps1

---
+++ PowerShell/10.9/Slack/Get-SSSlackConfiguration.ps1
@@ -0,0 +1,36 @@
+﻿###########################################################################################################################################################
+# Created By: Cyberhill Partners, LLC.
+# https://cyberhillpartners.com
+# Date: 2/10/2021
+# Description: Get the Slack Configuration from Secret Server
+###########################################################################################################################################################
+
+Function Get-SSSlackConfiguration {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $URI,
+         [Parameter(Mandatory=$true, Position=1)]
+         [string] $APIKey
+    )
+
+    try
+    {
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "Bearer $APIKey")
+    $URI = "$URI/api/v1/slack-configuration"
+    
+    $Slack = Invoke-RestMethod $URI -Method Get -Headers $headers
+    return $Slack
+    }
+    catch
+    {
+        $result = $_.Exception.Response.GetResponseStream();
+        $reader = New-Object System.IO.StreamReader($result);
+        $reader.BaseStream.Position = 0;
+        $reader.DiscardBufferedData();
+        $responseBody = $reader.ReadToEnd() | ConvertFrom-Json
+        Write-Host "ERROR: ($responseBody.error)"
+        return;
+    }
+}
